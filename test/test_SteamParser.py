358138bd55fc90b5b839a748ce5b2b873e2f6ed3
boyfws <minkin.d.d@gmail.com>
2025-01-03 03:12:06 +0300

    Добавил тесты

---
+++ test/test_SteamParser.py
@@ -0,0 +1,23 @@
+from main import SteamParser
+
+import aiohttp
+import pytest
+
+
+@pytest.mark.parametrize("num_pages", [5, 10, 15, 100])
+@pytest.mark.asyncio
+async def test_def_state(num_pages):
+    data = await SteamParser().parse(num_pages=num_pages)
+
+    for el in data:
+        assert len(el) == 14
+        assert len(el["developer"]) > 0
+        assert len(el["developer"]) == len(el["developer_link"])
+
+        if el["publisher"] is not None:
+            assert len(el["publisher"]) > 0
+            assert len(el["publisher"]) == len(el["publisher_link"])
+
+
+
+
